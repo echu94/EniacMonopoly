ade245c8e0b1747f3966e77bc326b08d8fa0a5b0
MIA^^ <miazhaouw@gmail.com>
2014-03-30 17:37:04 -0400

    models added

---
+++ src/models/models.go
@@ -0,0 +1,94 @@
+package models
+
+import (
+	"image/color"
+)
+
+type Player struct {
+	IsHuman           bool
+	Cash              int
+	Order             int
+	Properties        []Property
+	Token             Tokens
+	Space             Space
+	JailedTurn        int
+	HasJailFreeChance bool
+	HasJailFreeChest  bool
+}
+
+type Tokens int
+
+const (
+	Battleship = iota
+	Dog
+	Iron
+	RaceCar
+	Shoe
+	Thimble
+	TopHat
+	Wheelbarrow
+)
+
+type Space struct {
+	Name     string
+	Position int
+}
+
+type OwnableSpace struct {
+	Space
+	Owner          Player
+	IsMonopoly     bool
+	MortgageRate   int
+	UnmortgageRate int
+	Cost           int
+}
+
+type Utility struct {
+	OwnableSpace
+}
+
+type RailRoad struct {
+	OwnableSpace
+}
+
+type Property struct {
+	OwnableSpace
+	Color       color
+	Upgrades    int
+	Rent        []int
+	UpgradeCost int
+}
+
+type Go struct {
+	Space
+}
+
+type CardType int
+
+const (
+	CommunityChestSpace = iota
+	ChanceSpace
+)
+
+type LuckCardSpace struct {
+	Space
+	Type CardType
+}
+
+type CommunityChest struct {
+	Title       string
+	Description string
+}
+
+type Chance struct {
+	Title       string
+	Description string
+}
+
+type Board struct {
+	CurrentTurn         int
+	ChanceCards         []Chance
+	CommunityChestCards []CommunityChestChest
+	Players             []Player
+	DoublesCount        int
+}
