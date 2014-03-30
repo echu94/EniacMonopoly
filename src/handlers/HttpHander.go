3904bf3f7b66d099efa3ac43d58902f8ac167126
Eric Chu <ericchu94@gmail.com>
2014-03-30 17:42:59 -0400

    Added basic web server capabilities

--- src/handlers/HttpHander.go
+++ src/handlers/HttpHander.go
@@ -1,9 +1,27 @@
 package handlers
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
+const base = "src/html"
+
 func HttpHandler(w http.ResponseWriter, r *http.Request) {
 
+	var path string
+	if r.URL.Path == "/" {
+		path = base + "/EniacMonopoly.html"
+	} else {
+		path = base + r.URL.Path
+	}
+
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println("Invalid web request: ", err)
+		return
+	}
+
+	w.Write(bytes)
 }
